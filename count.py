ac4199a99f22f66b44543a71e916c675fc916bde
Justin Elak <justin.elak@gmail.com>
2025-11-28 15:19:35 -0500

    updated count

--- count.py
+++ count.py
@@ -9,6 +9,10 @@ class Counter:
         self.num += 1
         self.label.config(text=self.num)
 
+    def reset(self):
+        self.num = 0
+        self.label.config(text=self.num)
+
 window = Tk()
 window.title("Hello World!")
 window.config(background="grey")
@@ -20,5 +24,8 @@ counter = Counter(text)
 
 button = Button(window, text="Click me", command=counter.increment)
 button.pack()
+
+button2 = Button(window, text="Reset", command=counter.reset)
+button2.pack()
 
 window.mainloop()
